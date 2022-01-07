d84cab732ce4490df6027047e233cb801d557b81
jo <uuu9214@naver.com>
2022-01-07 16:33:55 +0900

    k번째수구하기

---
+++ src/com/codingtest/level1/Knum.java
@@ -0,0 +1,31 @@
+package com.codingtest.level1;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Knum {
+    public int[] solution(int[] array, int[][] commands) {
+        int kNum=0;
+        List<Integer> kNumList = new ArrayList<>();
+
+        for (int[] command : commands){
+            List<Integer> list = new ArrayList<>();
+            for (int i = (command[0]-1);i<command[1];i++){
+                list.add(array[i]);
+            }
+
+            Collections.sort(list);
+            kNum=list.get(command[2]-1);
+            kNumList.add(kNum);
+        }
+
+        return kNumList.stream().mapToInt(i->i).toArray();
+    }
+
+    public static void main(String[] args) {
+        Knum k = new Knum();
+        int[] result =k.solution(new int[]{1,5,2,6,3,7,4}, new int[][]{{2,5,3}, {4,4,1}, {1,7,3}});
+    }
+}
--- src/com/codingtest/level1/MathGiveUp.java
+++ src/com/codingtest/level1/MathGiveUp.java
@@ -1,6 +1,64 @@
 package com.codingtest.level1;
 
+import java.util.*;
+import java.util.stream.Stream;
+
 public class MathGiveUp {
 
-    
+    public ArrayList<Integer> rand(int leng) {
+        ArrayList<Integer> arr = new ArrayList<>() ;
+
+        for (int i = 0; i < leng; i++) {
+            //int a=(int) Math.random() * 4 + 1;
+            Random random=new Random();
+            int a=random.nextInt(4)+1;
+            arr.add(a);
+
+        }
+        return arr;
+    }
+
+
+    public int[] solution(int[] answers) {
+        ArrayList<Integer> supo1=rand(answers.length);
+        ArrayList<Integer> supo2=rand(answers.length);
+        ArrayList<Integer> supo3=rand(answers.length);
+        int supo1Cnt=0;
+        int supo2Cnt=0;
+        int supo3Cnt=0;
+        List<Integer> arr=new ArrayList<>();
+
+        for (int i =0 ; i < answers.length; i++){
+            if (answers[i]==supo1.get(i)) {
+                supo1Cnt++;
+            }
+            if(answers[i]==supo2.get(i)){
+                supo2Cnt++;
+            }
+            if(answers[i]==supo3.get(i)){
+                supo3Cnt++;
+            }
+        }
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        map.put(1,supo1Cnt);
+        map.put(2,supo2Cnt);
+        map.put(3,supo3Cnt);
+
+        Set<Map.Entry<Integer,Integer>> entrySet = map.entrySet();
+        for (Map.Entry<Integer,Integer> en : entrySet ){
+            int max=0;
+            if (en.getValue() >=max){
+                arr.add(en.getKey());
+            }
+        }
+
+
+        return arr.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+        MathGiveUp m = new MathGiveUp();
+        m.solution(new int[]{1,2,3,4,5});
+        System.out.println(Math.random() * 5 + 1);
+    }
 }
