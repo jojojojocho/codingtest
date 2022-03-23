b8f90942d7501ba10e978a72284849d81ad35bb3
jo <uuu9214@naver.com>
2022-03-23 20:14:15 +0900

    다단계칫솔

--- src/com/codingtest/programmers/level2/test.java
+++ src/com/codingtest/programmers/level2/test.java
@@ -2,8 +2,8 @@ package com.codingtest.programmers.level2;
 
 public class test {
     public static void main(String[] args) {
-        String s = "34";
-        s.substring(0,2);
-        System.out.println(s);
+        String s = "010-0000-0000";
+        System.out.println(s.replaceAll("^01(0|1|[6-9])-(\\d{3}|\\d{4})-\\d{4}$","9"));
+
     }
 }
